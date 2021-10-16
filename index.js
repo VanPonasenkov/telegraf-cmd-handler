79c23c9092693c642263d16733ad4842040e537c
VanPonasenkov <herrmitbusen@gmail.com>
2021-10-16 19:25:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,8 @@ const HANDLERENUM = require("./handlerenum");
 const Command = require("./command");
 const Event = require("./event");
 const process = require("process");
-
-/**
- * @description Config Object (Modify by using setEdirs function)
- */
-let edirs = {
-  cmdDir: "",
-  cmdImportDIr: "",
-  evDir: "",
-  evImportDIr: "",
-};
-
+let pdir;
+let pidir;
 const findCommand = (cmdl, commandName) => {
   for (cmd of cmdl) {
     if (cmd.aliases && cmd.aliases.includes(commandName)) {
@@ -22,80 +13,29 @@ const findCommand = (cmdl, commandName) => {
   }
 };
 
-const findEv = (evl, evName) => {
-  for (ev of evl) {
-    if (ev[0] === evName) {
-      return ev[1];
-    }
-  }
-};
-
-const setEdirs = (cmdDir, cmdImportDIr, evDir, evImportDIr) => {
-  edirs = {
-    cmdDir,
-    cmdImportDIr,
-    evDir,
-    evImportDIr,
-  };
-};
-
-const getEdirs = () => {
-  return edirs;
-};
-
-const getEvents = (dirs) => {
-  let events = new Map();
-  const eventFiles = fs.readdirSync(dirs.evDir);
-  for (const file of eventFiles) {
-    const event = require(`${dirs.evImportDIr}${file}`);
-    const eo = new event();
-    events.set(eo.type, eo);
-  }
-  return events;
-};
-
-const getCommands = (dirs) => {
-  let commands = new Map();
-  const commandFiles = fs.readdirSync(dirs.evDir);
-  for (const file of commandFiles) {
-    const command = require(`${dirs.evImportDIr}${file}`);
-    const co = new command();
-    commands.set(co.name, co);
-  }
-  return commands;
-};
-
-function execStickerEv(ctx, evl) {
-  findEv(evl, "sticker").execute(ctx);
-}
-
-function execPhotoEv(ctx, evl) {
-  findEv(evl, "photo").execute(ctx);
-}
-
-function execAudioEv(ctx, evl) {
-  findEv(evl, "audio").execute(ctx);
-}
-
 /**
  * @param {*} bot telegraf instance
  * @param {number} handlertype type of the handler, pass by using HANDLERENUM values, HANDLERENUM.COMMAND by default
+ * @param {string} dir represents the directory path, '${__dirname}\\commands' by default
+ * @param {string} importdir represents the directory path from which files are imported, './commands/' by default
  */
-
-const handlerInit = (bot, handlertype = HANDLERENUM.COMMAND) => {
+if(process.platform == "win32") {pdir = `${process.cwd()}\\commands`; pidir = `${process.cwd()}/commands/`}
+else if (process.platform == "linux" || process.platform == "freebsd" || process.platform == "openbsd" || process.platform == "darwin") { pdir = `${process.cwd()}/commands`; pidir = `${process.cwd()}/commands/`}
+const handlerInit = (
+  bot,
+  handlertype = HANDLERENUM.COMMAND,
+  dir = pdir,
+  importdir = pidir
+) => {
   if (handlertype == HANDLERENUM.COMMAND) {
-    let commands = getCommands(getEdirs);
+    let commands = new Map();
+    const commandFiles = fs.readdirSync(dir);
+    for (const file of commandFiles) {
+      const command = require(`${importdir}${file}`);
+      const co = new command();
+      commands.set(co.name, co);
+    }
     bot.on("message", (ctx) => {
-      let eventsl = getEvents(getEdirs());
-      if (ctx.message.sticker) {
-        execStickerEv(ctx, eventsl);
-        return;
-      } else if (ctx.message.photo) {
-        execPhotoEv(ctx, eventsl);
-        return;
-      } else if (ctx.message.audio) {
-      }
-
       const message = ctx.message;
       const args = message.text.slice(1).trim().split(/ +/);
       const commandName = args.shift().toLowerCase();
@@ -103,35 +43,38 @@ const handlerInit = (bot, handlertype = HANDLERENUM.COMMAND) => {
       const command =
         commands.get(commandName) || findCommand(commands, commandName);
 
-      if (!command) return;
+      if (!command) {
+
+        return
+      };
       if (command.args && !args.length) {
         return;
       }
       try {
         command.execute(ctx, message, args);
-        return;
       } catch (error) {
         console.error(error);
       }
     });
+  } else if (handlertype == HANDLERENUM.EVENT) {
+    let events = new Map();
+    const eventFiles = fs.readdirSync(dir);
+    for (const file of eventFiles) {
+      const event = require(`${importdir}${file}`);
+      const eo = new event();
+      events.set(eo.name, eo);
+    }
+    events.forEach((ev) => {
+      bot.on(ev.type, (ctx) => {
+        try {
+          ev.execute(ctx);
+        } catch (error) {
+          console.error(error);
+        }
+      });
+    });
+  } else {
+    throw new Error("Wrong handler type, use types in HANDLERENUM!");
   }
-  let events = getEvents(getEdirs());
-  events.forEach((ev) => {
-    bot.on(ev.type, (ctx) => {
-      try {
-        ev.execute(ctx);
-      } catch (error) {
-        console.error(error);
-      }
-    });
-  });
 };
-
-module.exports = {
-  handlerInit,
-  HANDLERENUM,
-  Command,
-  Event,
-  setEdirs,
-  getEdirs, // for debugging purposes only
-};
+module.exports = { handlerInit, HANDLERENUM, Command, Event };
